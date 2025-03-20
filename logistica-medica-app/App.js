f34de22aba88cf6c16bd7acf2715a94f4403aa40
ViniciusOtoni <viniciusotonimsv@gmail.com>
2025-03-19 23:35:06 -0300

    Adicionando as novas telas de login nas rotas.

--- logistica-medica-app/App.js
+++ logistica-medica-app/App.js
@@ -1,16 +1,21 @@
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import HomeScreen from './screens/HomeScreen';
-import DetailsScreen from './screens/DetailsScreen';
+import LoginAdmin from './src/screens/admin/LoginAdmin';
+import LoginScreen from './src/screens/user/Login';
+import SignUp from './src/screens/user/SignUp';
+import HomeChoice from './src/screens/user/HomeChoice';
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Home">
-        <Stack.Screen name="Home" component={HomeScreen} />
-        <Stack.Screen name="Details" component={DetailsScreen} />
+      <Stack.Navigator initialRouteName="HomeChoice">
+        <Stack.Screen name="HomeChoice" component={HomeChoice} />
+        <Stack.Screen name="SignUp" component={SignUp} />
+        <Stack.Screen name="LoginAdmin" component={LoginAdmin} />
+        <Stack.Screen name="Login" component={LoginScreen} />
+        
       </Stack.Navigator>
     </NavigationContainer>
   );
