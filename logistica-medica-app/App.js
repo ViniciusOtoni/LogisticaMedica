3573308a2f0551d7339c2087bb6dd60d538b90e5
ViniciusOtoni <viniciusotonimsv@gmail.com>
2025-03-20 19:57:43 -0300

    Adicionando novas retas.

--- logistica-medica-app/App.js
+++ logistica-medica-app/App.js
@@ -5,6 +5,12 @@ import LoginScreen from './src/screens/user/Login';
 import SignUp from './src/screens/user/SignUp';
 import HomeChoice from './src/screens/user/HomeChoice';
 import MainScreen from './src/screens/user/MainScreen';
+import HomePage from './src/screens/user/HomePage';
+import ListOrders from './src/screens/user/ListOrders';
+import NewOrder from './src/screens/user/NewOrder';
+import OrderDetails from './src/screens/user/OrderDetails';
+import ListOrdersAdmin from './src/screens/admin/ListOrdersAdmin';
+import OrderConclusion from './src/screens/admin/OrderConclusion';
 
 const Stack = createNativeStackNavigator();
 
@@ -17,7 +23,12 @@ export default function App() {
         <Stack.Screen name="LoginAdmin" component={LoginAdmin} options={{title: ''}} />
         <Stack.Screen name="Login" component={LoginScreen} options={{title: ''}} />
         <Stack.Screen name="MainScreen" component={MainScreen} options={{title: ''}}/>
-        
+        <Stack.Screen name="HomePage" component={HomePage} options={{title: ''}}/>
+        <Stack.Screen name="ListOrders" component={ListOrders} options={{title: ''}}/>
+        <Stack.Screen name="NewOrder" component={NewOrder} options={{title: ''}}/>
+        <Stack.Screen name="OrderDetails" component={OrderDetails} options={{title: ''}}/>
+        <Stack.Screen name="ListOrdersAdmin" component={ListOrdersAdmin} options={{title: ''}}/>
+        <Stack.Screen name="OrderConclusion" component={OrderConclusion} options={{title: ''}}/>
       </Stack.Navigator>
     </NavigationContainer>
   );
