66401c2e41d2581614afb764cf67b623635978ac
ViniciusOtoni <viniciusotonimsv@gmail.com>
2025-03-27 21:59:29 -0300

    Refatorando telas de pedido adicionando scroll para melhor dimensionamento.

--- logistica-medica-app/src/screens/admin/OrderConclusion.js
+++ logistica-medica-app/src/screens/admin/OrderConclusion.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { View, StyleSheet, Text } from "react-native";
+import { View, StyleSheet, Text, ScrollView } from "react-native";
 import details from "../../../assets/images/details-icon.png";
 
 import HeaderTitle from "../../components/HeaderTitle";
@@ -11,96 +11,101 @@ import Title from "../../components/Title";
 
 const OrderConclusion = () => {
   return (
-    <View style={styles.container}>
-      <View style={styles.topContainer}>
-        <HeaderTitle
-          text={"Detalhes do Pedido"}
-          color="#FFFFFF"
-          icon={details}
-        />
+    <ScrollView contentContainerStyle={styles.scrollContainer}>
+      <View style={styles.container}>
+        <View style={styles.topContainer}>
+          <HeaderTitle
+            text={"Detalhes do Pedido"}
+            color="#FFFFFF"
+            icon={details}
+          />
+        </View>
+        <View style={styles.bottomContainer}>
+          <CustomCard text={"Previsão de Entrega:"} borderColor="#DC1111">
+            <View style={styles.padding}>
+              <Text>Data: {"18/12/2005"}</Text>
+            </View>
+            <View style={[styles.row, styles.padding]}>
+              <View>
+                <Text> Laboratório 01 → </Text>
+              </View>
+              <View>
+                <Text> Laboratório 15 </Text>
+              </View>
+            </View>
+            <View style={[styles.row, styles.padding]}>
+              <View>
+                <Text> Destinatário: </Text>
+              </View>
+              <View>
+                <Text> XXX </Text>
+              </View>
+            </View>
+            <View style={styles.padding}>
+              <Text> Endereço: Rua XXX nº 10 - São Paulo, SP </Text>
+            </View>
+            <View style={styles.padding}>
+              <Text> CEP: 00000-000 </Text>
+            </View>
+            <View style={styles.row}>
+              <View>
+                <Text> Responsável: </Text>
+              </View>
+              <View>
+                <Text> XXX </Text>
+              </View>
+            </View>
+            <View style={[styles.row, styles.padding]}>
+              <View>
+                <Text> Horário Coleta: </Text>
+              </View>
+              <View>
+                <Text> XXX </Text>
+              </View>
+            </View>
+            <View style={styles.padding}>
+              <Text> Temperatura da Coleta: {"XXX"} </Text>
+            </View>
+            <View style={styles.titleWrapper}>
+              <Title text={"Adicionar Evidência:"} />
+            </View>
+            <View>
+              <ImageInput />
+            </View>
+            <View style={styles.buttons}>
+              <View style={styles.buttonWrapper}>
+                <CustomButton
+                  text={"Concluir Pedido"}
+                  textColor="#11DC18"
+                  backgroundColor="transparent"
+                  borderColor="#11DC18"
+                  borderWidth={2}
+                />
+              </View>
+              <View style={styles.buttonWrapper}>
+                <CustomButton
+                  text={"Voltar"}
+                  textColor="#DC1111"
+                  backgroundColor="transparent"
+                  borderColor="#DC1111"
+                  borderWidth={2}
+                />
+              </View>
+            </View>
+            <View>
+              <CustomSectionLabel label={"Urgente"} color="#DC1111" />
+            </View>
+          </CustomCard>
+        </View>
       </View>
-      <View style={styles.bottomContainer}>
-        <CustomCard text={"Previsão de Entrega:"} borderColor="#DC1111">
-          <View style={styles.padding}>
-            <Text>Data: {"18/12/2005"}</Text>
-          </View>
-          <View style={[styles.row, styles.padding]}>
-            <View>
-              <Text> Laboratório 01 → </Text>
-            </View>
-            <View>
-              <Text> Laboratório 15 </Text>
-            </View>
-          </View>
-          <View style={[styles.row, styles.padding]}>
-            <View>
-              <Text> Destinatário: </Text>
-            </View>
-            <View>
-              <Text> XXX </Text>
-            </View>
-          </View>
-          <View style={styles.padding}>
-            <Text> Endereço: Rua XXX nº 10 - São Paulo, SP </Text>
-          </View>
-          <View style={styles.padding}>
-            <Text> CEP: 00000-000 </Text>
-          </View>
-          <View style={styles.row}>
-            <View>
-              <Text> Responsável: </Text>
-            </View>
-            <View>
-              <Text> XXX </Text>
-            </View>
-          </View>
-          <View style={[styles.row, styles.padding]}>
-            <View>
-              <Text> Horário Coleta: </Text>
-            </View>
-            <View>
-              <Text> XXX </Text>
-            </View>
-          </View>
-          <View style={styles.padding}>
-            <Text> Temperatura da Coleta: {"XXX"} </Text>
-          </View>
-          <View style={styles.titleWrapper}>
-            <Title text={"Adicionar Evidência:"} />
-          </View>
-          <View>
-            <ImageInput />
-          </View>
-          <View style={styles.buttons}>
-            <View style={styles.buttonWrapper}>
-              <CustomButton
-                text={"Concluir Pedido"}
-                textColor="#11DC18"
-                backgroundColor="transparent"
-                borderColor="#11DC18"
-                borderWidth={2}
-              />
-            </View>
-            <View style={styles.buttonWrapper}>
-              <CustomButton
-                text={"Voltar"}
-                textColor="#DC1111"
-                backgroundColor="transparent"
-                borderColor="#DC1111"
-                borderWidth={2}
-              />
-            </View>
-          </View>
-          <View>
-            <CustomSectionLabel label={"Urgente"} color="#DC1111" />
-          </View>
-        </CustomCard>
-      </View>
-    </View>
+    </ScrollView>
   );
 };
 
 const styles = StyleSheet.create({
+  scrollContainer: {
+    flexGrow: 1, // Garante que o conteúdo ocupe a tela inteira, permitindo a rolagem se necessário
+  },
   container: {
     flex: 1,
     backgroundColor: "#119FDC",
--- logistica-medica-app/src/screens/user/ListOrders.js
+++ logistica-medica-app/src/screens/user/ListOrders.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { View, StyleSheet, Text } from 'react-native';
+import { View, StyleSheet, Text, ScrollView } from 'react-native';
 import { useNavigation } from "@react-navigation/native";
 
 import HeaderTitle from "../../components/HeaderTitle";
@@ -13,98 +13,102 @@ const ListOrders = () => {
   const navigation = useNavigation();
 
   return (
-    <View style={styles.container}>
-      <View style={styles.topContainer}>
-        <HeaderTitle icon={analysing} text={"Monitoramento"} color="#FFF" />
-      </View>
-
-      <View style={styles.bottomContainer}>
-        <View>
-          <LabeledInput placeholder={"Pesquisar"} />
+    <ScrollView contentContainerStyle={styles.scrollContainer}>
+      <View style={styles.container}>
+        <View style={styles.topContainer}>
+          <HeaderTitle icon={analysing} text={"Monitoramento"} color="#FFF" />
         </View>
 
-        <CustomButton
-          text="Gerar Relatório"
-          textColor="#119FDC"
-          backgroundColor="transparent"
-          borderColor="#119FDC"
-          borderWidth={2}
-          onPress={() => navigation.navigate("")}
-        />
+        <View style={styles.bottomContainer}>
+          <View>
+            <LabeledInput placeholder={"Pesquisar"} />
+          </View>
 
-        <CustomSectionLabel label={"Pendente"} color="#DC1111" />
+          <CustomButton
+            text="Gerar Relatório"
+            textColor="#119FDC"
+            backgroundColor="transparent"
+            borderColor="#119FDC"
+            borderWidth={2}
+            onPress={() => navigation.navigate("")}
+          />
 
-        <CustomCard
-          text={"Previsão de Entrega: 19/12/1997"}
-          borderColor="#DC1111"
-          onPress={() => navigation.navigate('OrderDetails')}
-        >
-          <View style={[styles.row, styles.padding]}>
-            <View>
-              <Text style={styles.bold}> Laboratório 01 → </Text>
+          <CustomSectionLabel label={"Pendente"} color="#DC1111" />
+
+          <CustomCard
+            text={"Previsão de Entrega: 19/12/1997"}
+            borderColor="#DC1111"
+            onPress={() => navigation.navigate('OrderDetails')}
+          >
+            <View style={[styles.row, styles.padding]}>
+              <View>
+                <Text style={styles.bold}> Laboratório 01 → </Text>
+              </View>
+              <View>
+                <Text style={styles.bold}> Laboratório 15 </Text>
+              </View>
             </View>
-            <View>
-              <Text style={styles.bold}> Laboratório 15 </Text>
+
+            <View style={styles.padding}>
+              <Text> Destinatário: </Text>
             </View>
-          </View>
-
-          <View style={styles.padding}>
-            <Text> Destinatário: </Text>
-          </View>
-          <View style={styles.padding}>
-            <Text> Laboratório 15 </Text>
-          </View>
-          <View style={styles.padding}>
-            <Text> Endereço: Rua XXX nº 10 - São Paulo, SP </Text>
-          </View>
-          <View style={styles.padding}>
-            <Text> CEP: 00000-000 </Text>
-          </View>
-        </CustomCard>
-
-        <CustomSectionLabel label={"Concluído"} color="#11DC18" />
-
-        <CustomCard
-          text={"Previsão de Entrega: 20/03/2025"}
-          borderColor="#11DC18"
-          onPress={() => navigation.navigate('OrderDetails')}
-        >
-
-          <View style={[styles.row, styles.padding]}>
-            <View>
-              <Text style={styles.bold}> Laboratório 01 → </Text>
+            <View style={styles.padding}>
+              <Text> Laboratório 15 </Text>
             </View>
-            <View>
-              <Text style={styles.bold}> Laboratório 15 </Text>
+            <View style={styles.padding}>
+              <Text> Endereço: Rua XXX nº 10 - São Paulo, SP </Text>
             </View>
-          </View>
+            <View style={styles.padding}>
+              <Text> CEP: 00000-000 </Text>
+            </View>
+          </CustomCard>
 
-          <View style={styles.padding}>
-            <Text> Destinatário: </Text>
-          </View>
-          <View style={styles.padding}>
-            <Text> Laboratório 15 </Text>
-          </View>
-          <View style={styles.padding}>
-            <Text> Endereço: Rua XXX nº 10 - São Paulo, SP </Text>
-          </View>
-          <View style={styles.padding}>
-            <Text> CEP: 00000-000 </Text>
-          </View>
-        </CustomCard>
+          <CustomSectionLabel label={"Concluído"} color="#11DC18" />
+
+          <CustomCard
+            text={"Previsão de Entrega: 20/03/2025"}
+            borderColor="#11DC18"
+            onPress={() => navigation.navigate('OrderDetails')}
+          >
+            <View style={[styles.row, styles.padding]}>
+              <View>
+                <Text style={styles.bold}> Laboratório 01 → </Text>
+              </View>
+              <View>
+                <Text style={styles.bold}> Laboratório 15 </Text>
+              </View>
+            </View>
+
+            <View style={styles.padding}>
+              <Text> Destinatário: </Text>
+            </View>
+            <View style={styles.padding}>
+              <Text> Laboratório 15 </Text>
+            </View>
+            <View style={styles.padding}>
+              <Text> Endereço: Rua XXX nº 10 - São Paulo, SP </Text>
+            </View>
+            <View style={styles.padding}>
+              <Text> CEP: 00000-000 </Text>
+            </View>
+          </CustomCard>
+        </View>
       </View>
-    </View>
+    </ScrollView>
   );
 };
 
 const styles = StyleSheet.create({
+  scrollContainer: {
+    flexGrow: 1, // Garante que o conteúdo ocupe toda a altura da tela, permitindo a rolagem
+  },
   container: {
     flex: 1,
     backgroundColor: "#119FDC",
   },
   topContainer: {
     marginTop: 16,
-    alignItems: "left",
+    alignItems: "flex-start",
     paddingVertical: 10,
     paddingHorizontal: 20,
   },
